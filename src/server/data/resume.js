5ea9fdd46fad9447eb79fd4d41da322a2728dfb4
Carlos Carvalho <carlos.carvalho@nbrsolutions.pt>
2019-02-01 17:43:20 +0000

    First Commit

---
+++ src/server/data/resume.js
@@ -0,0 +1,25 @@
+// TODO: Change to go to Linkedin
+
+export default [
+  {
+    company: 'NBRSolutions',
+    title: 'Software Engineer',
+    location: 'Porto',
+    started: 'Nov 2014',
+    finished: ''
+  },
+  {
+    company: 'Orbcom',
+    title: 'Web Developer / Consultant',
+    location: 'Porto',
+    started: 'Feb 2013',
+    finished: 'Oct 2014'
+  },
+  {
+    company: 'ITSector',
+    title: 'Software Developer',
+    location: 'Porto, Portugal',
+    started: 'Mar 2008',
+    finished: 'Jan 2013'
+  }
+]
